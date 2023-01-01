ba18fdc6fff82cc79f55f169316e1a0415ebc5bc
dark-noob83 <mahdiamr83@gmail.com>
2023-01-01 21:26:48 +0330

    update

--- bot.js
+++ bot.js
@@ -1,42 +1,68 @@
 const {Telegraf}= require('telegraf');
 const bot =new Telegraf('5654638506:AAHZyVj5ASXw_cUBAyyADwGCnsj5yfgw9Ok');
-const { Keyboard ,Key} = require('telegram-keyboard')
+const { Keyboard } = require('telegram-keyboard')
 
 const db=[]
-
+const keyboard = Keyboard.make([
+    ['پروفایل من👤'],
+    ['تماس با مدیر📞'], 
+    ['ثبت نام 📜'],
+])
 bot.start(ctx=>{
     bot.telegram.sendMessage(ctx.chat.id,'سلام خوش امدید',{
     })
+    
+  //ctx.reply('dar khedmatm', keyboard.inline()) 
+         ctx.reply('در خدمتم', keyboard.reply())   
 })
 
 
-bot.command('member',ctx=>{
+
+
+bot.hears('پروفایل من👤',ctx=>{
+     id=ctx.from.username
+     if(ctx.from.username==undefined){
+        id='ایدی موجود نمیباشد'
+     }
+    let name=ctx.from.first_name
+    g=0
+    var emtiaz=0;
+    for (let index = 0; index < db.length; index++){
+        if (db[index].name==ctx.from.id ){
+            g++;
+            emtiaz =  db[index].count
+        }
+    }
+    if(g==0){
+        emtiaz=0;
+    }
+    const money=10000*emtiaz
+    
     console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id,"list of memebers")
-})
-
-
+    bot.telegram.sendMessage(ctx.chat.id,`اسم شما : ${name} \n ایدی شما : ${id} \n امتیاز شما : ${emtiaz} \n حساب شما : ${money} \n`)
+  });
+  bot.hears('تماس با مدیر📞',ctx=>{
+    
+        bot.telegram.sendMessage(ctx.chat.id,'ایدی ادمین : \n https://t.me/Arman3831')
+    
+  });
+  bot.hears('ثبت نام 📜',ctx=>{
+        
+        bot.telegram.sendMessage(ctx.chat.id,'register')
+    
+  });
 bot.on('message',ctx=>{
     // console.log(ctx.message)
-    const keyboard = Keyboard.make([
-    Key.callback('پروفایل من👤','profile'),
-    Key.callback('تماس با مدیر📞','admin'), 
-    Key.callback('ثبت نام 📜','register'),
-  ]).inline()
+    
 
 
-  if (ctx.message.text==='👤پروفایل من'||
-    ctx.message.text==='تماس با مدیر📞'||
-    ctx.message.text==='ثبت نام 📜'){
-    //    ctx.reply('dar khedmatm', keyboard.inline()) 
-       ctx.reply('dar khedmatm', keyboard.reply())   
-  }
-
-
-  bot.action('profile',ctx=>{
-    console.log('salam')
-  })
-
+//  if (ctx.message.text==='👤پروفایل من'||
+//     ctx.message.text==='تماس با مدیر📞'||
+//     ctx.message.text==='ثبت نام 📜')
+//     {
+//         ctx.reply('dar khedmatm', keyboard.inline()) 
+//          ctx.reply('dar khedmatm', keyboard.reply())   
+//     }
 
     if (ctx.message.new_chat_members!=undefined){
         g=0
@@ -55,5 +81,7 @@ bot.on('message',ctx=>{
         
     }
 })
+
+ 
 
 bot.launch();
