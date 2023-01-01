ad1c60d13a250f978e2f9dc7915274ff5408565c
NiceAss2003 <113842809+NiceAss2003@users.noreply.github.com>
2023-01-01 21:47:17 +0330

    update

--- bot.js
+++ bot.js
@@ -1,24 +1,21 @@
 const {Telegraf}= require('telegraf');
 const bot =new Telegraf('5654638506:AAHZyVj5ASXw_cUBAyyADwGCnsj5yfgw9Ok');
 const { Keyboard } = require('telegram-keyboard')
-
 const db=[]
+// for adding keyboard button
 const keyboard = Keyboard.make([
     ['پروفایل من👤'],
     ['تماس با مدیر📞'], 
     ['ثبت نام 📜'],
 ])
+// start bot
 bot.start(ctx=>{
     bot.telegram.sendMessage(ctx.chat.id,'سلام خوش امدید',{
-    })
-    
-  //ctx.reply('dar khedmatm', keyboard.inline()) 
+    }) 
          ctx.reply('در خدمتم', keyboard.reply())   
 })
 
-
-
-
+// action button of my profile
 bot.hears('پروفایل من👤',ctx=>{
      id=ctx.from.username
      if(ctx.from.username==undefined){
@@ -37,33 +34,23 @@ bot.hears('پروفایل من👤',ctx=>{
         emtiaz=0;
     }
     const money=10000*emtiaz
-    
     console.log(ctx.from)
     bot.telegram.sendMessage(ctx.chat.id,`اسم شما : ${name} \n ایدی شما : ${id} \n امتیاز شما : ${emtiaz} \n حساب شما : ${money} \n`)
   });
+
+//   action button for contact with admin
   bot.hears('تماس با مدیر📞',ctx=>{
-    
-        bot.telegram.sendMessage(ctx.chat.id,'ایدی ادمین : \n https://t.me/Arman3831')
-    
+       bot.telegram.sendMessage(ctx.chat.id,'ایدی ادمین : \n https://t.me/Arman3831')
+
+// action button for register
   });
-  bot.hears('ثبت نام 📜',ctx=>{
-        
+  bot.hears('ثبت نام 📜',ctx=>{  
         bot.telegram.sendMessage(ctx.chat.id,'register')
-    
+        
   });
-bot.on('message',ctx=>{
-    // console.log(ctx.message)
-    
 
-
-//  if (ctx.message.text==='👤پروفایل من'||
-//     ctx.message.text==='تماس با مدیر📞'||
-//     ctx.message.text==='ثبت نام 📜')
-//     {
-//         ctx.reply('dar khedmatm', keyboard.inline()) 
-//          ctx.reply('dar khedmatm', keyboard.reply())   
-//     }
-
+// adding member 
+    bot.on('message',ctx=>{
     if (ctx.message.new_chat_members!=undefined){
         g=0
         for (let index = 0; index < db.length; index++){
@@ -81,7 +68,5 @@ bot.on('message',ctx=>{
         
     }
 })
-
  
-
 bot.launch();
