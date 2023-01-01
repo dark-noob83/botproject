0cca810091be6b4d729c8a84a819bdf7a40538e4
NiceAss2003 <113842809+NiceAss2003@users.noreply.github.com>
2023-01-01 14:23:53 +0330

    Update

--- bot.js
+++ bot.js
@@ -1,13 +1,43 @@
 const {Telegraf}= require('telegraf');
 
 const bot =new Telegraf('5654638506:AAHZyVj5ASXw_cUBAyyADwGCnsj5yfgw9Ok');
-
+const db=[]
 bot.start(ctx=>{
-    bot.telegram.sendMessage(ctx.chat.id,'salam khosh amadid',{
+    bot.telegram.sendMessage(ctx.chat.id,'سلام خوش امدید',{
 
     })
 })
 
+bot.command('member',ctx=>{
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id,"list of memebers")
+})
 
+bot.on('message',ctx=>{
+    
+    if (ctx.message.new_chat_members==undefined) {
+        console.log('kesi add nashod')
+    }
+    else{
+        g=0
+        for (let index = 0; index < db.length; index++) {
+            if (db[index].name==ctx.from.id) {
+                db[index].count++
+                g++;
+            }
+        }
+        if (g!=0){
+            db.push({
+                name:ctx.message.from.id,
+                count:1
+            })
+        }
+        console.log(ctx.message.from)
+        for (let i = 0; i< db.length; i++) {
+            const element = db[i];
+            console.log(element)
+        }
+    }
+})
 
 bot.launch();
